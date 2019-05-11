34be59646a6f967509aec602e00625b3d9c70456
yufeng0924 <cho61183@gmail.com>
2019-05-11 12:25:49 +0800

    week6

--- week6/machine-learning-ex5/ex5/linearRegCostFunction.m
+++ week6/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -18,17 +18,12 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
+%[0;theta(2:end)] 这里的theta 从 1 开始.也就是i的初始值. 忽略theta[0]
+predictions = X * theta;
+sqrErrors = (predictions - y).^2;
+J = (1/(2*m)) * sum(sqrErrors) + lambda/(2*m) * sum([0;theta(2:end)] .^ 2);
 
-
-
-
-
-
-
-
-
-
-
+grad = (1/m) * X' * (predictions-y)  + (lambda / m ) * [0;theta(2:end)]
 
 % =========================================================================
 
