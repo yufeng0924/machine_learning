dd9c9de708ace6a22ea0e3d4d8f95687c8b8a057
yufeng0924 <cho61183@gmail.com>
2019-06-02 09:51:33 +0800

    wee9

--- week9/machine-learning-ex8/ex8/cofiCostFunc.m
+++ week9/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -45,7 +45,17 @@ Theta_grad = zeros(size(Theta));
 
 
 
+diff = (X*Theta'-Y);
+J = sum((diff.^2)(R==1))/2;
+J = J + lambda*sum(sum(Theta.^2))/2;  % regularized term of theta.
+J = J + lambda*sum(sum(X.^2))/2;     % regularized term of x.
 
+X_grad = (diff.*R)*Theta;                 %unregularized vectorized implementation
+Theta_grad = ((diff.*R)'*X);              %unregularized vectorized implementation
+
+
+X_grad = X_grad + (lambda * X);             % regularized
+Theta_grad = Theta_grad + (lambda * Theta);  % regularized
 
 
 
